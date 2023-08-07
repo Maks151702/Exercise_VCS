8631c4176b9b531bd2759ceb069b7c6838491963
Maks <chubar.2003h@gmail.com>
2023-08-07 20:32:40 +0300

    Создал новую ветку

---
+++ src/MyWood/Door.java
@@ -0,0 +1,7 @@
+package MyWood;
+
+public class Door {
+    int weight = 100;
+    int height = 200;
+
+}
---
+++ src/MyWood/Main.java
@@ -0,0 +1,9 @@
+package MyWood;
+
+public class Main {
+
+    public static void main(String[]args){
+
+        int height = 10;
+    }
+}
